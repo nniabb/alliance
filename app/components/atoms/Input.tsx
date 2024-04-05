ca3632454c9da526ffc234f47308c2f435f600af
nniabb <niabilanishvili17@gmail.com>
2024-04-05 17:13:46 +0400

    final commit

--- app/components/atoms/Input.tsx
+++ app/components/atoms/Input.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 
 interface InputProps {
     placeholder: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; 
+    type: string;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void; 
 }
 
-const Input: React.FC<InputProps> = ({placeholder, onChange}) => {
+const Input: React.FC<InputProps> = ({placeholder, onChange, type}) => {
     return (
         <input 
             placeholder={placeholder}
-            onChange={onChange} 
-            className="border-b border-transparent hover:border-gray-500 outline-none w-[250px]" />
+            onChange={onChange}
+            type={type}
+            className="border-b border-transparent hover:border-gray-500 outline-none w-[250px] [&::-webkit-inner-spin-button]:appearance-none" />
     )
 }
  
+
 export default Input;
+
--- app/components/molecules/UserInfo.tsx
+++ app/components/molecules/UserInfo.tsx
@@ -5,34 +5,42 @@ import ButtonEdit from '../atoms/Button';
 
 interface UserInfoProps {}
 
-const UserInfo: React.FC<UserInfoProps> = ({}) => {
+const UserInfo: React.FC<UserInfoProps> = () => {
   const [email, setEmail] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('+995');
-  const [language, setLanguage] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+  const [selectedCountryCode, setSelectedCountryCode] = useState<string>('+995')
+  const [language, setLanguage] = useState<string>('')
 
   const allInputsFilled = () => {
     return email.trim() !== '' && phoneNumber.trim() !== '' && language.trim() !== '';
   };
 
+  const handlePhoneNumberChange = (value: string) => {
+    setPhoneNumber(selectedCountryCode + value)
+  };
+
   return (
     <div className="flex flex-col gap-[5px]">
-      <p className="text-gray-500 text-sm mt-[8px]">Email Address</p>
-      <Input placeholder="enter email" onChange={(e) => setEmail(e.target.value)} />
-      <p className="text-gray-500 text-sm mt-[8px]">Phone Number</p>
-      <div className="flex">
-        <PhoneNumberSelect onChange={(value) => setPhoneNumber(value)} />
-        <Input placeholder="enter number" onChange={(e) => setPhoneNumber(e.target.value)} />
-      </div>
-      <p className="text-gray-500 text-sm mt-[8px]">Choose Language</p>
-      <Input placeholder="choose language" onChange={(e) => setLanguage(e.target.value)} />
-      
-      {allInputsFilled() && (
-        <div className='flex mt-[20px] ml-[80%]'>
-         <ButtonEdit title='save' />
+      <form className="flex flex-col gap-[5px]">
+        <label className="text-gray-500 text-sm mt-[8px]">Email Address</label>
+        <Input type='email' placeholder="Enter Email" onChange={(e) => setEmail(e.target.value)} />
+        <label className="text-gray-500 text-sm mt-[8px]">Phone Number</label>
+        <div className="flex gap-2">
+          <PhoneNumberSelect onChange={(value) => setSelectedCountryCode(value)} />
+          <Input type='number' placeholder="Enter Number" onChange={(e) => handlePhoneNumberChange(e.target.value)} />
         </div>
-      )}
+        <label className="text-gray-500 text-sm mt-[8px]">Choose Language</label>
+        <Input type='text' placeholder="Choose Language" onChange={(e) => setLanguage(e.target.value)} />
+
+        {allInputsFilled() && (
+          <div className='flex mt-[20px] ml-[80%]'>
+            <ButtonEdit title='save' />
+          </div>
+        )}
+      </form>
     </div>
   );
 };
 
 export default UserInfo;
+
